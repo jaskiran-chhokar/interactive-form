113587d96f646a2cdac2f783eb8a79ee3c4285fa
Jaskiran <jaskiranchhokar@gmail.com>
2020-03-24 20:03:38 -0400

    Set focus on first input onload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//Set focus on first text field
+document.addEventListener('DOMContentLoaded', () => {
+    const firstInput = document.querySelector('#name'); 
+    firstInput.focus();
+}); 
+
+//'Other' job role functionality
+
+
+
