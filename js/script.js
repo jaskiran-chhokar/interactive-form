9356fe1deb3df5045c1db6746e3f301c0df80e9b
Jaskiran <jaskiranchhokar@gmail.com>
2020-03-24 20:38:33 -0400

    Add functionality for 'other' job role option

--- js/script.js
+++ js/script.js
@@ -5,6 +5,23 @@ document.addEventListener('DOMContentLoaded', () => {
 }); 
 
 //'Other' job role functionality
+const otherJobRole = () => {
+    const jobRole = document.querySelector('select[id=title]');
+    const otherJob = document.querySelector('#other-title');
+
+    //Set input to display none, by default
+    otherJob.style.display = 'none';
+
+    jobRole.addEventListener('change', e => {
+        if(e.target.value === 'other') {
+            otherJob.style.display = 'block';
+        } else {
+            otherJob.style.display = 'none';
+        }
+    });
+}
+
+otherJobRole();
 
 
 
