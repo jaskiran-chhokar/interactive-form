2a338a848dad084e67ae60db0e6fbed9b7af328c
Jaskiran <jaskiranchhokar@gmail.com>
2020-03-25 21:51:12 -0400

    add functionality for tshirt section

--- js/script.js
+++ js/script.js
@@ -23,5 +23,47 @@ const otherJobRole = () => {
 
 otherJobRole();
 
+//T-shirt info section
+const tShirtInfo = () => {
+    // const selectTheme = document.querySelector('.select-theme');
+    // const themeJsPuns = document.querySelector('.js-puns-theme');
+    // const themeLoveJs = document.querySelector('.heart-js-theme');
+    const color = document.querySelector('#color');
+    const colorOptions = document.querySelectorAll('#color > option');
+    
 
+
+       // colorOptions[i].style.display = 'none';
+
+        color.addEventListener('change', e => {
+
+            for(let i = 0; i < colorOptions.length; i++) {
+
+                if(e.target.className === 'js-puns-theme') {
+
+                         console.log('dfasfasdf');
+
+            //     if(colorOptions[i].textContent.includes('(JS Puns shirt only)')) {
+            //         colorOptions[i].style.display = 'block';
+            //         console.log(colorOptions[i]);
+            //    }        
+
+                } 
+
+            }
+
+        });
+
+
+
+
+        // if(themeLoveJs.selected = 'true') {
+        //     if(colorOptions[i].textContent.includes('(I &#9829; JS shirt only)')) {
+        //         colorOptions[i].style.display = 'block';
+        //    }
+        // }
+
+}
+
+tShirtInfo();
 
